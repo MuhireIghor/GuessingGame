f447a219631daba0727bf1406738c215af8444a0
MuhireIghor <muhireighor123@gmail.com>
2022-10-24 21:43:21 +0200

    guessing feature

--- game.cpp
+++ game.cpp
@@ -1,18 +1,20 @@
 #include<iostream>
 #include<cstring>
 #include<time.h>
+#include<cstdlib>
 using namespace std;
 int main(){
 	int i;
 	int choice ;
-	string guessed;
+	char guessed;
+	int a,p,f,l,sa,sp,sf,sl;
 	srand ( time(NULL));
-	string animals[5] = {"Elephant","Monkey","Tiger","Zebra","Antelope"};
+	string animals[5] = {"elephant","monkey","tiger","zebra","zntelope"};
 	string places[5]={"Kigali","Nairobi","Kampala","Bujumbura","Dar-es-salaam"};
 	string food[5]={"Banana","Apple","DODO","Rice","Cabbage"};
 	string people[5]={"Johnson","Dickson","Ericson","FredEson","Nickson"};
-		int randIndex = rand () % 5;
-		string fortune;
+	int randIndex = rand () % 5;
+	string fortune;
 		
 	
 	cout<<"Welcome to the guessing game : "<<endl;
@@ -29,37 +31,90 @@ int main(){
 	
 switch(choice){
 	case 1:
-		cout<<"You have chose the "<<category[choice-1] <<" category ";
+		cout<<"You have chosen the "<<category[choice-1] <<" category "<<endl;
+		init:
     	cout<<"Enter any letter of your choice : "<<endl;
-	    cin>>guessed;
-	    fortune = animals[randIndex];
-	    cout<<fortune;
+	    cin>>guessed;	  
+	    for(a=0;a<5;a++){
+	    	    for(sa=0;sa<animals[a].length();){
+	    	if(guessed != animals[a][sa]){
+	    		cout<<"- ";
+			}
+			else{	
+				cout<<animals[a][sa];
+			}
+			sa++;
+//			continue;
+//		cout<<"wow";
+	    		}
+goto init;	
+		break;
+			}
+		
+	    
+	    
+	  
 		break;
 	case 2:
 		cout <<" You have chosen the "<<category[choice-1] << " category ";
 	    cout<<"Enter any letter of your choice : "<<endl;
 	    cin>>guessed;
-	    fortune = places[randIndex];
-	    cout<<fortune;
+	 	    for(p=0;p<5;p++){
+	    	    for(sp=0;sl<places[p].length();sp++){
+	    	if(guessed != places[p][sp]){
+	    	cout<<"-";
+			}
+			else{
+				cout<<guessed;	
+					}
+	
+		}
+		break;
+			}
+		
 		break;
     case 3:
         cout <<" You have chosen the "<<category[choice-1] << " category "<<endl;    
 	    cout<<"Enter any letter of your choice : ";
 	    cin>>guessed;
-	    fortune = food[randIndex];
-	    cout<<fortune;
+	    for(f=0;f<5;f++){
+	   	    for(sf=0;sf<food[f].length();sf++){
+	    	if(guessed != food[f][sf]){
+	    		cout<<"-";
+			}
+			else{	
+				cout<<guessed;
+			}	
+		
+		}
+		break;
+			}
+		
     	break;
     case 4:
     	cout <<" You have chosen the "<<category[choice-1] << " category "<<endl;
        	cout<<"Enter any letter of your choice : ";
     	cin>>guessed;
-    	fortune = food[randIndex];
-    	cout<<fortune;
+    	    for(a=0;a<5;a++){
+	    for(sl=0;sl<people[l].length();sl++){
+	    	if(guessed != people[l][sl]){
+	    			cout<<"-";
+			}
+			else{
+				cout<<guessed;	
+		
+			}
+			
+		}
+		break;
+			}
+			
     	break;
-    		
+		}
+    	
     
     	
-}
+
 
 	
 	
